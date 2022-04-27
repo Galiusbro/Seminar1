51eac34571f26e519a654c6e618041a1a2f6684d
Galprim <galprimulus@gmail.com>
2022-04-27 23:33:42 +0300

    Task2 solution

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите первое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2) Console.WriteLine($"max = {number1}");
+else Console.WriteLine($"max = {number2}");
