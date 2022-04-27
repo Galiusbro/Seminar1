bf20bc5ee54f83e5185699778db7d9a8ea0cb01e
Galprim <galprimulus@gmail.com>
2022-04-27 23:47:39 +0300

    Task6 solution

---
+++ Task6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Ввкдите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number % 2 == 0) Console.WriteLine("Число яыляется четным");
+else Console.WriteLine("Число яыляется НЕчетным");
